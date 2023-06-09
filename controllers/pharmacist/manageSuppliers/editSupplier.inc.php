03849e1274c05330c8ab0c5b5bf3abc37b87691c
Mo2024 <mohdosama2025@gmail.com>
2023-06-09 13:42:32 +0300

    edit supplier finished

---
+++ controllers/pharmacist/manageSuppliers/editSupplier.inc.php
@@ -0,0 +1,67 @@
+<?php 
+require('../../functions/functions.inc.php');
+require("../../partials/regex.inc.php");
+if(isset($_SESSION['userId'])){
+    if($_SESSION['role'] == "pharmacist"){
+        if(isset($_GET['supplierId'])){
+            $sid = $_GET['supplierId'];
+            $sidQuery = "SELECT * FROM suppliers WHERE sid = '$sid'";
+            $result = $db->query($sidQuery);
+            $row = $result->fetch();
+        }else{
+            $_SESSION['error'] = "Choose a valid Supplier";
+            header("Location: /pharmacy-management-system/pharmacist/manageSuppliers/suppliersList.php");
+        }
+
+        if(isset($_POST['submit'])){
+            $sid = $_POST['submit'];
+            $name = $_POST['name'];
+            $building = $_POST['building'];
+            $road = $_POST['road'];
+            $block = $_POST['block'];
+
+            $nameValid = preg_match($nameReg, $name);
+            $buildingValid = preg_match('/^[a-zA-Z0-9\s]+$/', $building);
+            $roadValid = preg_match('/^[a-zA-Z0-9\s]+$/', $road);
+            $blockValid = preg_match('/^[a-zA-Z0-9\s]+$/', $block); 
+
+            if (!$nameValid) {
+                $_SESSION['error'] = "Invalid name";
+                header("Location: /pharmacy-management-system/pharmacist/manageSuppliers/editSupplier.php?supplierId=".$sid);
+            } elseif (!$buildingValid) {
+                $_SESSION['error'] = "Invalid building";
+                header("Location: /pharmacy-management-system/pharmacist/manageSuppliers/editSupplier.php?supplierId=".$sid);
+            } elseif (!$roadValid) {
+                $_SESSION['error'] = "Invalid road";
+                header("Location: /pharmacy-management-system/pharmacist/manageSuppliers/editSupplier.php?supplierId=".$sid);
+            } elseif (!$blockValid) {
+                $_SESSION['error'] = "Invalid block";
+                header("Location: /pharmacy-management-system/pharmacist/manageSuppliers/editSupplier.php?supplierId=".$sid);
+            }else{
+                $insertQuery = "UPDATE suppliers SET name = :name, building = :building, road = :road, block = :block WHERE sid = :sid";
+                $stmt = $db->prepare($insertQuery);
+                $stmt->bindParam(':sid', $sid);
+                $stmt->bindParam(':name', $name);
+                $stmt->bindParam(':road', $road);
+                $stmt->bindParam(':building', $building);
+                $stmt->bindParam(':block', $block);
+                $stmt->execute();
+
+                $_SESSION['success'] = "Supplier's Info Updated";
+                header("Location: /pharmacy-management-system/pharmacist/manageSuppliers/SuppliersList.php");
+            }
+            
+        }
+
+    }else{
+        $_SESSION['error'] = "Unauthorized user";
+        header("Location: /pharmacy-management-system/mainpage.php");
+    }
+    
+}else{
+    $url = 'http://'.$_SERVER['HTTP_HOST'].$_SERVER['REQUEST_URI'];
+    setcookie("redirect", $url,0,'/');
+    header("Location: /pharmacy-management-system/auth/signin.php");
+}
+
+?>
---
+++ pharmacist/manageSuppliers/editSupplier.php
@@ -0,0 +1,38 @@
+<?php $title = "Add Branch"; require('../../partials/boilerplate.inc.php')?>
+<?php require('../../controllers/pharmacist/manageSuppliers/editSupplier.inc.php')?>
+
+<section class="container row mx-auto">
+    <div class="col-lg-12">
+        <h1 class="text-dark">Edit Supplier</h1>
+        <form method="POST" class="validated-form" novalidate enctype="multipart/form-data">
+
+            <div class="mb-3">
+                <label class="form-label text-dark" for="name">Name</label>
+                <input placeholder="Name" class="form-control" type="text" name="name" id="name" value="<?php echo $row['name']; ?>" required>
+            </div>
+
+            <div class="mb-3">
+                <label class="form-label text-dark" for="building">Building</label>
+                <input placeholder="Building" class="form-control" type="text" name="building" id="building" value="<?php echo $row['building']; ?>" required>
+            </div>
+
+            <div class="mb-3">
+                <label class="form-label text-dark" for="road">Road</label>
+                <input placeholder="Road" class="form-control" type="text" name="road" id="road" value="<?php echo $row['road']; ?>" required>
+            </div>
+
+            <div class="mb-3">
+                <label class="form-label text-dark" for="block">Block</label>
+                <input placeholder="Block" class="form-control" type="text" name="block" id="block" value="<?php echo $row['block']; ?>" required>
+            </div>
+
+            <div class="mb-3">
+                <a class="text-decoration-none btn btn-warning" href="/pharmacy-management-system/pharmacist/manageSuppliers/SuppliersList.php">Return</a>
+                <button name="submit" value="<?php echo $_GET['supplierId'] ?>" class="btn btn-success">Save Info</button>
+            </div>
+        </form>
+    </div>
+</section>
+
+
+<?php require('../../partials/footer.inc.php')?>
