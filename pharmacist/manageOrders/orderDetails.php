d60f4da649cf9277c679b5eb3d81f61099021ab1
Mo2024 <mohdosama2025@gmail.com>
2023-06-10 18:21:15 +0300

    orders list and details workingg

---
+++ pharmacist/manageOrders/orderDetails.php
@@ -0,0 +1,36 @@
+<?php $title = "Order Details"; require('../../partials/boilerplate.inc.php')?>
+<?php require('../../controllers/pharmacist/manageOrders/ordersDetails.inc.php')?>
+
+<div class="container mb-3" style="margin-top:30px;">
+<div class="row justify-content-center">
+    <div class="col-xl-9">
+        <div class="bg-color border card border-color-form shadow">
+            <div class="card-body">
+            <h5 class="card-title text-center" style="font-size:45px;font-weight:bold;color:black;">Order Details</h5>
+            <ul class="list-group list-group-flush">
+              <li class="list-group-item"><h5>Order No: </h5><?php echo $row['oid'] ?></li>
+                <li class="list-group-item"> <h5>Price: </h5><?php echo $row['totalPrice'] ?></li>
+                <li class="list-group-item"><h5>Payment Method: </h5><?php echo $row['paymentMethod'] ?></li>
+                <li class="list-group-item"><h5>Date of Order: </h5><?php echo $row['orderDate'] ?></li>
+                <li class="list-group-item"><h5>Status: </h5><?php echo $row['status'] ?></li>
+                <?php if($row['status'] == 'delivered'){ ?>
+                    
+                    <li class="list-group-item"><h5>Date of Delivery:</h5><?php echo $row['dateDelivered'] ?></li>
+                    <?php }?>
+                <li class="list-group-item"><h5>Orderer Name: </h5><?php echo $row['fName'] ?></li>
+                <li class="list-group-item"><h5>Building: </h5><?php echo $row['building'] ?></li>
+                <li class="list-group-item"><h5>Road: </h5><?php echo $row['road'] ?></li>
+                <li class="list-group-item"><h5>Block: </h5><?php echo $row['block'] ?></li>
+             </ul>
+             
+            </div>
+        </div>
+        <div class="d-flex justify-content-end mt-3">
+        <a class="text-decoration-none btn btn-sm btn-outline-primary ms-auto" href="/pharmacy-management-system/pharmacist/manageOrders/ordersList.php">Return To Orders List</a>
+        </div>
+    </div>
+</div>
+</div>
+
+
+<?php require('../../partials/footer.inc.php')?>
---
+++ pharmacist/manageOrders/ordersList.php
@@ -0,0 +1,48 @@
+<?php $title = "Manage Orders"; require('../../partials/boilerplate.inc.php')?>
+<?php require('../../controllers/pharmacist/manageOrders/ordersList.inc.php')?>
+<style>
+thead
+{
+    background-color: #3C486B;
+    color: white;
+}
+</style>
+<div class="mt-4 container">
+  <div class="table-responsive">
+    <table class="table table-bordered border border-dark text-center">
+
+      <thead>
+        <tr >
+          <th  scope="col" colspan="4">Orders List</th>
+        </tr>
+      </thead>
+
+      <tbody>
+
+        <tr>
+          <th>Order No</th>
+          <th>Date</th>
+          <th>Details</th>
+          <th>Update Status</th>
+        </tr>
+        
+        <?php foreach ($rows as $row) { ?>
+          <tr>
+            <td><?php echo $row['oid']; ?></td>
+            <td><?php echo $row['orderDate'];?></td>
+            <td><a href="/pharmacy-management-system/pharmacist/manageOrders/orderDetails.php?orderId=<?php echo $row['oid'] ?>" type="button" class="btn btn-sm btn-primary">Details</a></td>
+            <td><a type="button" href="/pharmacy-management-system/pharmacist/manageOrders/updateOrder.php?orderId=<?php echo $row['oid'] ?>" class="btn btn-sm btn-outline-secondary">Update Order</a></td>
+          </tr>
+        <?php } ?>
+
+      </tbody>
+
+      </table>
+    </div>
+    <div class="d-flex mb-3">
+      <button type="button" class="ms-auto border-0">
+        <a class="text-decoration-none btn btn-sm btn-outline-primary ms-auto" href="/pharmacy-management-system/mainpage.php">Return To Home Page</a>
+      </button>
+    </div>
+</div>
+<?php require('../../partials/footer.inc.php')?>
