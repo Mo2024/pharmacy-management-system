b165229e55cd004b22a83afddaf1539c952792a0
Mo2024 <mohdosama2025@gmail.com>
2023-06-11 21:39:05 +0300

    qty changing fixed

--- cart.php
+++ cart.php
@@ -1,4 +1,17 @@
 <?php $title = "Cart"; require('partials/boilerplate.inc.php')?>
+<style>
+thead
+@media (max-width: 576px) {
+  .w-xs-50{
+    width: 50%
+  }
+}
+@media (min-width: 576px) {
+  .w-md-50{
+    width: 27.5%
+  }
+}
+</style>
 <script>
     function getQuantityByPID(productsArray, pid) {
   const product = productsArray.find(item => parseInt(item.pid) === pid);
@@ -19,8 +32,6 @@ xhr.onreadystatechange = function () {
             }else{
                 let cart = JSON.parse(response);
                 let localCart = JSON.parse(localStorage.getItem('cart'))
-                console.log(cart);
-
                 let mainContainer = document.getElementById('mainContainer');
                 for(const item of cart){
 
@@ -35,7 +46,7 @@ xhr.onreadystatechange = function () {
                         }
                         let qty = getQuantityByPID(localCart, item.pid)
                         let itemDiv = 
-                        `<div class="card mb-3 mt-3 w-75">
+                        `<div id="div${item.pid}" class="card mb-3 mt-3 w-75">
                             <div class="row">
                             <div class="col-md-3">
                                 <img src="${src}" alt="" class="img-fluid h-100 w-100">
@@ -44,6 +55,15 @@ xhr.onreadystatechange = function () {
                                     <div class="card-body">
                                         <h3 class="card-title">${item.name}</h3>
                                         <h4 class="card-title">Total Price: $${item.price*qty}</h4>
+                                        <div class="input-group w-50">
+                                            <span class="input-group-btn">
+                                            <button type="button" class="btn btn-outline-secondary" onclick="decreaseValue(${item.pid})">-</button>
+                                            </span>
+                                            <input id="${item.pid}" type="number" class="form-control qty text-center"  value="${qty}" onchange="handleQtyUpdate(this.id, this.value)"  min="1">
+                                            <span class="input-group-btn">
+                                            <button type="button" class="btn btn-outline-secondary" onclick="increaseValue(${item.pid})">+</button>
+                                            </span>
+                                        </div>
                                     </div>
                                 </div>
                             </div>
@@ -75,6 +95,51 @@ xhr.onerror = function () {
 var data = "cart=" + encodeURIComponent(localStorage.getItem('cart'));
 xhr.send(data);
 
+function handleQtyUpdate(id, qty) {
+  let cart = JSON.parse(localStorage.getItem('cart'));
+  let updatedCart = [];
+
+  if (qty <= 0) {
+    // Delete div and remove item from cart
+    let divId = 'div' + id;
+    let divElement = document.getElementById(divId);
+    if (divElement) {
+      divElement.remove();
+    }
+
+    // Remove item from cart based on 'pid' value
+    updatedCart = cart.filter((item) => parseInt(item.pid) !== id);
+  } else {
+    // Update quantity in cart based on 'pid' value
+    updatedCart = cart.map((item) => {
+      if (parseInt(item.pid) === id) {
+        return { ...item, qty: qty };
+      }
+      return item;
+    });
+  }
+
+  // Update cart in local storage
+  localStorage.setItem('cart', JSON.stringify(updatedCart));
+}
+
+function increaseValue(id) {
+  let qtyInput = document.getElementById(id);
+  if (qtyInput) {
+    let qty = parseInt(qtyInput.value) + 1;
+    qtyInput.value = qty;
+    handleQtyUpdate(id, qty);
+  }
+}
+
+function decreaseValue(id) {
+  let qtyInput = document.getElementById(id);
+  if (qtyInput) {
+    let qty = parseInt(qtyInput.value) - 1;
+    qtyInput.value = qty;
+    handleQtyUpdate(id, qty);
+  }
+}
 </script>
 
 <div  class="container">
