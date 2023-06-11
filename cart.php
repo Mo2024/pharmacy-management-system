965f4b22e02e140d877fd8795161d8589c31cd84
Mo2024 <mohdosama2025@gmail.com>
2023-06-11 21:15:31 +0300

    cart page progress

--- cart.php
+++ cart.php
@@ -24,20 +24,38 @@ xhr.onreadystatechange = function () {
                 let mainContainer = document.getElementById('mainContainer');
                 for(const item of cart){
 
-                    let qty = getQuantityByPID(localCart, item.pid)
-                    let itemDiv = 
-                    `<div class="card mb-3 mt-3">
-                        <div class="row">
-                            <div class="col-md-8 mt-5">
-                                <div class="card-body">
-                                    <h3 class="card-title">${item.name}</h3>
-                                    <h4 class="card-title">Total Price: ${item.price*qty}</h4>
+                    var imageUrl = '/pharmacy-management-system/public/products/' + item.pid + '.jpg';
+                    fetch(imageUrl, { method: 'HEAD' })
+                    .then(function(response) {
+                        let src = '';
+                        if (response.ok) {
+                            src = '/pharmacy-management-system/public/products/' + item.pid + '.jpg';
+                        } else {
+                            src = '/pharmacy-management-system/public/imgs/no.png';
+                        }
+                        let qty = getQuantityByPID(localCart, item.pid)
+                        let itemDiv = 
+                        `<div class="card mb-3 mt-3 w-75">
+                            <div class="row">
+                            <div class="col-md-3">
+                                <img src="${src}" alt="" class="img-fluid h-100 w-100">
+                            </div>
+                                <div class="col-md-8 mt-5">
+                                    <div class="card-body">
+                                        <h3 class="card-title">${item.name}</h3>
+                                        <h4 class="card-title">Total Price: $${item.price*qty}</h4>
+                                    </div>
                                 </div>
                             </div>
-                        </div>
-                    </div>`
-                    
-                    mainContainer.innerHTML += itemDiv;
+                        </div>`
+                        
+                        mainContainer.innerHTML += itemDiv;
+                    })
+                    .catch(function(error) {
+                        // Error occurred during the request
+                        console.log('Error:', error);
+                    });
+
 
                 }
             }
@@ -59,6 +77,9 @@ xhr.send(data);
 
 </script>
 
-<div id="mainContainer" class="container" >
+<div  class="container">
+<div id="mainContainer" class="row justify-content-center">
+
+</div>
 </div>
 <?php require('partials/footer.inc.php')?>
